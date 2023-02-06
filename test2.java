e49f546d73902b97c672c2f57ff8c5b4b6cb302f
D55ma <100362815+D55ma@users.noreply.github.com>
2023-02-06 15:46:02 +0300

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,13 @@
+class test2 {
+	public static void main(String args[])
+	{
+		int x=7;
+        
+		public static Boolean checkSomething() {
+    if (x>3) {
+        boolean boolValue =true;
+        return boolValue;
+    } else return null;
+}
+	}
+}
